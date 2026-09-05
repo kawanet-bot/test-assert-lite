f206784b3bb80aeff41606cc721a4090aed804b3
Yusuke Kawasaki <u-suke@kawa.net>
2026-09-06 01:02:59 +0900

    move: src/reporter/

--- src/reporter/spec.test.ts
+++ src/reporter/spec.test.ts
@@ -1,6 +1,6 @@
 import {strict as assert} from "node:assert"
 import {describe, it} from "node:test"
-import {createTAL} from "./index.ts"
+import {createTAL} from "../index.ts"
 
 const TITLE = "spec.test.ts"
 
--- src/reporter/spec.ts
+++ src/reporter/spec.ts
@@ -1,5 +1,5 @@
 import type * as declared from "test-assert-lite"
-import {errorText, isSubtestsFailed} from "./common/test-failure.ts"
+import {errorText, isSubtestsFailed} from "../common/test-failure.ts"
 
 type TestEvent = declared.TAL.TestEvent
 type FormatFn = declared.TAL.FormatFn
