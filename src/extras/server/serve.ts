3d220dd75864b7125232413738a0cada805a5fdf
Yusuke Kawasaki <u-suke@kawa.net>
2026-09-22 18:08:36 +0900

    Revert "Refactor server cleanup to use Promise for close"
    This reverts commit bc1a89fae1983ff97639bfa87964ec555cd7fd7c.

--- src/extras/server/serve.ts
+++ src/extras/server/serve.ts
@@ -122,8 +122,8 @@ export const serve = async ({handler, quiet, services, ...options}: ServeOptions
     })
 
     services.onCleanup(async () => {
-        await new Promise<void>(resolve => server.close(() => resolve()))
-        await sleep(0)
+        server.close()
+        await sleep(1)
         server.closeAllConnections()
     })
 
